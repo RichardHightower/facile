3dc35a6a4ef81a000d824df46c83ef88e43787b5
Rick <richardhightower@gmail.com>
2012-06-21 02:50:26 -0700

    some more todos

--- main/src/org/facile/TodoAndExperiments.java
+++ main/src/org/facile/TodoAndExperiments.java
@@ -8,12 +8,19 @@ public class TodoAndExperiments {
 	//TODO # create a shift function like Perl's, look at Python pop, push. It should work with arrays and lists.
 	//TODO # Add an ARGS holder for the main method. See if this is already a system property. 
 	//TODO # Extract useful System properties into a class called System, which is system done right.
-	//TODO # What if you create a file context like Perl's <IN>. This context could work with regex too like $1 (group 1 in split), $_ (line), @_ (array), next (regex), and unless (regex)
 	//TODO # create len, _int, _float, _double, functions (rejected scalar, but scalar could be used for length of array, collection, and conversion of wrapper to primitive)
 	//TODO # create len, _int, _float, _double, converters
-	//TODO # the name enumerate kind of sucks... Perl, Groovy use forEach, Ruby uses each. I like enumerate the least.
-	//TDOO # Change enumerate to each. It is shorter and more descriptive
+	//TODO # the name enumerate kind of sucks... Perl, Groovy use forEach, Ruby uses each. I like enumerate the least. Change enumerate to each. It is shorter and more descriptive
 	//TODO # Groovy uses find and findAll, we use filter like Python. We should create a find and findAll too.
-	//TODO # A printIf command might be nice. Like print but does this Perlism print STDERR "$httperf_command\n" if $DEBUG; would look like printIf(DEBUG, STDERR, "blah blah");
-	//TODO # Perl treats running a process like opening a file where the output of the process is a stream. I like this. 
+	//TODO # Ability to extract properties from lists... i.e., salary of each employee. extract(depts, {dept.getEmployee()}).extract({emp.getSalary}); (where blocks are functions)
+	//TODO # Safe get safeGet({employee.getAddress().getPhone()});
+
+	
+	
+	//DONE ITEMS
+	//DONE # Perl treats running a process like opening a file where the output of the process is a stream. I like this. 
+	//DONE # What if you create a file context like Perl's <IN>. This context could work with regex too like $1 (group 1 in split), $_ (line), @_ (array), next (regex), and unless (regex)
+	
+	//IDEA: File parsing might be a good first application of Facile, use it to parse files.
+
 }
